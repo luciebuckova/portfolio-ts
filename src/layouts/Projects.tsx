f910d068390e05a1623eaf1a6ac02087e86d3090
Lucie Bučková <lucie.buckova@gmail.com>
2024-05-07 16:30:34 +0200

    Remove projects section

--- src/layouts/Projects.tsx
+++ src/layouts/Projects.tsx
@@ -5,55 +5,7 @@ export default function Projects() {
   return (
     <Element name="projects" className="py-16 md:py-32">
       <h3>Projekty</h3>
-      <p>
-        Tady je pár projektů, na kterých jsem pracovala, a také technologie,
-        které jsem používala. Více malých i vetších projektů najdete na mém{' '}
-        <a
-          href="https://github.com/luciebuckova/"
-          className="text-teal-400 duration-300 hover:text-purple-600"
-          aria-label="Github">
-          GitHubu
-        </a>
-        .
-      </p>
-      <ul className="mt-8 grid gap-8 md:grid-cols-2 lg:grid-cols-3">
-        <li>
-          <Card
-            title="👩‍💻 Tento web"
-            description="Můj osobní web vytvořený pomocí Reactu a Next.js, na kterém dále pracuji."
-            linkWeb="https://portfolio-ts-theta-hazel.vercel.app/"
-            linkRepo="https://github.com/luciebuckova/portfolio-ts"
-            stack={['React', 'TypeScript', 'Tailwind CSS', 'Next.js', 'MUI']}
-          />
-        </li>
-        <li>
-          <Card
-            title="😷 Zdravotnické potřeby"
-            description="Webové stránky pro prodejnu zdravotnických potřeb, na kterých aktuálně ještě pracuji."
-            linkWeb="https://zdravotnicke-potreby.vercel.app/"
-            linkRepo="https://github.com/luciebuckova/zdravotnicke-potreby"
-            stack={['React', 'TypeScript', 'Tailwind CSS']}
-          />
-        </li>
-        <li>
-          <Card
-            title="🚲 RTB 2023"
-            description="Závěrečný projekt kurzu Staň se kodérkou - webové stránky fiktivního cyklistického závodu."
-            linkWeb="https://rtb-race.netlify.app/"
-            linkRepo="https://github.com/luciebuckova/final-project"
-            stack={['HTML', 'SCSS', 'Eleventy']}
-          />
-        </li>
-        <li>
-          <Card
-            title="🧹 Hodinová nemanželka"
-            description="Webové stránky pro služby úklidu v domácnostech."
-            linkWeb="https://hodinova-nemanzelka.cz/"
-            linkRepo="https://github.com/luciebuckova/nemanzelka"
-            stack={['HTML', 'SCSS', 'JavaScript']}
-          />
-        </li>
-      </ul>
+      <p>Na této sekci právě pracuji... 🙃</p>
     </Element>
   );
 }
